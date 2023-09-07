a7becf911ef03eb36e05271d876873afd0bb2813
Filipe Merli <filipemerli@gmail.com>
2023-09-07 16:47:14 +0100

    XCode project creation

---
+++ ForecastApp/ForecastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForecastApp
+//
+//  Created by Filipe Merli on 07/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
